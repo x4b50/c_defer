e8b50d8dcb346f35ab3b419734e2273c28de9c8e
x4b50 <k.piwowarski@proton.me>
2024-03-20 15:36:25 +0100

    removed old impementation

--- defer.c
+++ defer.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// /*
 #define DEFER_IMPL
 #include "defer.h"
 
@@ -42,29 +41,3 @@ int main() {
     printf("-------------\n");
     return_dbg(0);
 }
-// */
-
-/*
-#define defer(v, e) e; free(v);
-#define defers(t, n, v, e) {t *n = v; e; free(n);}
-int main() {
-    int *t = malloc(sizeof(int)*100);
-    defer(t, {
-        for (int i=0; i<10; i++) {
-            t[i] = i;
-            printf("%d ", t[i]);
-        }
-        printf("\n%d\n", t[0]);
-    })
-    printf("%d\n", t[0]);
-
-    defers(int, tst, calloc(100, sizeof(int)), {
-        for (int i=0; i<10; i++) {
-            tst[i] += i;
-            printf("%d ", tst[i]);
-        }
-        printf("\n%d\n", tst[0]);
-    })
-    // printf("%d\n", tst[0]);
-}
-// */
