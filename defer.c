5bf0ca771dd36274d1fbfd5c2a5f4ec6e18959bb
x4b50 <k.piwowarski@proton.me>
2024-03-20 15:18:23 +0100

    made it work with allocations inside other functions

--- defer.c
+++ defer.c
@@ -1,30 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // /*
 // can be replaced w/ linked list if you want a more versatile solution
 #define DEFER_COUNT 32
-void * __defer_list[DEFER_COUNT];
-int __defer_count = 0;
+#define DEFER_NAME_LEN 128
+typedef struct {
+    char fname[DEFER_NAME_LEN];
+    void *list[DEFER_COUNT];
+    int count;
+} __defer_list;
 
-static inline void defer(void *const v) {
-    __defer_list[__defer_count] = v;
-    __defer_count += 1;
+typedef struct __def_node {
+    __defer_list val;
+    struct __def_node *next;
+} __def_node;
+
+void __dl_push(__def_node *head, __defer_list val) {
+    __def_node *current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    current->next = (__def_node*) malloc(sizeof(__def_node));
+    current->next->val = val;
+    current->next->next = NULL;
 }
 
-#define return(v) \
-    for (int _i=__defer_count-1; _i>=0; _i--) { \
-        free(__defer_list[_i]); \
-    } __defer_count=0; return v
+void __dl_pop(__def_node *head) {
+    if (head->next == NULL) {
+        free(head);
+    }
+    __def_node *current = head;
+    while (current->next->next != NULL) {
+        current = current->next;
+    }
+    free(current->next);
+    current->next = NULL;
+}
+
+__defer_list *__dl_last(__def_node *head) {
+    __defer_list *ret;
+    if (head->next == NULL) {
+        ret = &head->val;
+        return ret;
+    }
+    __def_node *current = head->next;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    ret = &current->val;
+    return ret;
+}
+
+__def_node __defers;
+
+#define defer(v) { \
+    __defer_list *_d = __dl_last(&__defers); \
+    char _func[DEFER_NAME_LEN]; \
+    strncpy(_func, __func__, 128); \
+    if (strcmp(_d->fname, _func) == 0) { \
+        _d->count += 1; \
+        _d->list[_d->count] = v; \
+    } else { \
+        __defer_list _d = {.list=v, .count=0}; \
+        strncpy(_d.fname, __func__, 128); \
+        __dl_push(&__defers, _d); \
+    } \
+}
+
+#define return(v) { \
+    __defer_list *_d = __dl_last(&__defers); \
+    char _func[DEFER_NAME_LEN]; \
+    strncpy(_func, __func__, 128); \
+    if (strcmp(_d->fname, _func) == 0) { \
+        for (int _i=_d->count; _i>=0; _i--) { \
+            free(_d->list[_i]); \
+        } \
+        __dl_pop(&__defers); \
+    } \
+} return v
 
 // just to demonstrate that it works
-#define return_dbg(v) \
-    for (int _i=__defer_count-1; _i>=0; _i--) { \
-        printf("%ld\n", (long)__defer_list[_i]); \
-        free(__defer_list[_i]); \
-    } __defer_count=0; return v
+#define return_dbg(v) { \
+    __defer_list *_d = __dl_last(&__defers); \
+    char _func[DEFER_NAME_LEN]; \
+    strncpy(_func, __func__, 128); \
+    if (strcmp(_d->fname, _func) == 0) { \
+        for (int _i=_d->count; _i>=0; _i--) { \
+            printf("%ld\n", (long)_d->list[_i]); \
+            free(_d->list[_i]); \
+        } \
+        __dl_pop(&__defers); \
+    } \
+} return v
 
-int empty_defer() {
+int test_defer() {
+    int *tst = calloc(100, sizeof(int));
+    defer(tst);
+    printf("%ld\n", (long)tst);
     return_dbg(1);
 }
 
@@ -52,9 +126,10 @@ int main() {
     printf("%ld\n", (long)t);
     printf("%ld\n", (long)t1);
     printf("%ld\n", (long)t2);
-    printf("-------------\n");
 
-    int _ = empty_defer();
+    printf("-------------\n");
+    int _ = test_defer();
+    printf("-------------\n");
     return_dbg(0);
 }
 // */
